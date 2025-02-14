247ecec0b2700ff94d22c15818084a4fa6d2aa6d
Dongmin <emthrology@gmail.com>
2025-02-14 19:33:24 +0900

    커스텀 훅에 doc 추가

--- util/useFormField.js
+++ util/useFormField.js
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 
+/**
+ * @description useState에 handleChange를 달아 form field의 관리를 수월하게 하기 위한 훅
+ * @param {*} initialValue
+ * @returns { object } value, setValue, handleChange
+ */
 export const useFormField = (initialValue = '') => {
   const [value, setValue] = useState(initialValue);
 
--- util/useValidation.js
+++ util/useValidation.js
@@ -1,5 +1,11 @@
 import { useState, useCallback } from 'react';
 
+/**
+ * @description 벨리데이션 훅
+ * @param { function } validateFn 벨리데이션에 샤용할 함수
+ * @param { string } initialErrorMsg 초기 에러메시지 ('')으로 보내도 됨
+ * @returns { object } isValid, errorMessage, validate(useCallbeck)
+ */
 export const useValidation = (validateFn, initialErrorMsg = '') => {
   const [isValid, setIsValid] = useState(true);
   const [errorMessage, setErrorMessage] = useState(initialErrorMsg);
