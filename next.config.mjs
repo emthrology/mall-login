7f31862d92e7eacec2a223ea8c8b8f5660eaa7d2
Dongmin <emthrology@gmail.com>
2025-03-04 16:10:06 +0900

    add: api config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    // 클라이언트와 서버 모두에서 접근 가능
+    apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
