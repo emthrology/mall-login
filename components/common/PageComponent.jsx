56de2c6c994a058c9348bb31c9e87f58e0c5e2a8
Dongmin <emthrology@gmail.com>
2025-03-06 16:03:49 +0900

    fix: pageComponent css 정리

--- components/common/PageComponent.jsx
+++ components/common/PageComponent.jsx
@@ -3,11 +3,9 @@ import Header from '@/components/common/Header';
 export default function PageComponent({ headerTitle, children }) {
   return (
     <div>
-      <div className="container mx-auto px-4 py-12 min-h-screen">
-        <div className="mx-w-screen-lg mx-auto">
-          <Header title={headerTitle} />
-          {children}
-        </div>
+      <div className="flex flex-col justify-center container mx-auto px-4 py-12 mx-w-screen-lg min-h-screen">
+        <Header title={headerTitle} />
+        {children}
       </div>
     </div>
   );
