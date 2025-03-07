e1c70b6062cdd1f30ac6f41616e09e5a8e2ca4f6
Dongmin <emthrology@gmail.com>
2025-03-07 19:45:24 +0900

    fix: 포스트맨 내용 반영

--- components/find/FindComponent.jsx
+++ components/find/FindComponent.jsx
@@ -7,13 +7,13 @@ import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 import Timer from '../common/Timer';
 
-const validatePhone = phone => {
+const validatePhone = (phone) => {
   if (!/^(\+82|0)(10|11|16|17|18|19)\d{7,8}$/.test(phone)) {
     return [false, '전화번호를 올바르게 입력해주세요'];
   }
   return [true, ''];
 };
-const validateCerti = certiVal => {
+const validateCerti = (certiVal) => {
   if (certiVal === '' || certiVal === null) {
     return [false, '인증번호를 입력해주세요'];
   }
@@ -22,25 +22,24 @@ const validateCerti = certiVal => {
 export default function FindComponent() {
   const router = useRouter();
   const phoneField = useFormField('');
-  const certiReqField = useFormField(false);
+  const certiReqField = useFormField(true);
   const certificationField = useFormField('');
   const phoneValidation = useValidation(
     validatePhone,
-    '전화번호를 입력해주세요',
+    '전화번호를 입력해주세요'
   );
   const certiValidation = useValidation(
     validateCerti,
-    '인증번호를 입력해주세요',
+    '인증번호를 입력해주세요'
   );
   const [counter, setCounter] = useState(180);
-  const [cert, setCert] = useState('');
 
   const requestCertNumber = async () => {
     const valid = phoneValidation.validate(phoneField.value);
     if (valid) {
       try {
         const response = await fetch(
-          `${publicRuntimeConfig.apiUrl}/cert/verify`,
+          `${publicRuntimeConfig.apiUrl}/cert/send`,
           {
             method: 'POST',
             headers: {
@@ -49,11 +48,11 @@ export default function FindComponent() {
             body: JSON.stringify({
               phone: phoneField.value,
             }),
-          },
+          }
         );
+        console.log(response, 3333);
         if (response.ok) {
           const data = await response.json();
-          setCert(data.cert);
           certiReqField.setValue(true);
           console.log(data, 666);
         } else {
@@ -77,42 +76,45 @@ export default function FindComponent() {
     }
   };
   const requestCert = async () => {
-    try {
-      const response = await fetch(
-        `${publicRuntimeConfig.apiUrl}/cert/verify?phone=${phoneField.value}`,
-        {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            certi_code: cert,
-          }),
-        },
-      );
-      if (response.ok) {
-        const data = await response.json();
-        // TODO activate this line
-        // certiReqField.setValue(true)
-        console.log(data, 666);
-        router.push(`/find/reset?userid=${data.loginId}`);
-      } else {
-        const error = await response.json();
-        alert(error.message);
+    const valid = certiValidation.validate(certificationField.value);
+    if (valid) {
+      try {
+        const response = await fetch(
+          `${publicRuntimeConfig.apiUrl}/cert/verify?phone=${phoneField.value}`,
+          {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              certi_code: certificationField.value,
+            }),
+          }
+        );
+        if (response.ok) {
+          const data = await response.json();
+          // TODO activate this line
+          // certiReqField.setValue(true)
+          console.log(data, 666);
+          router.push(`/find/reset?userid=${data.loginId}`);
+        } else {
+          const error = await response.json();
+          alert(error.message);
+        }
+      } catch (error) {
+        console.log(error, 131313);
+        // const contentType = error.headers.get('Content-Type') || '';
+        // if (contentType.includes('application/json')) {
+        //   const errorData = await error.json();
+        //   console.error('인증번호 전송 에러:', errorData);
+        //   return errorData;
+        // } else {
+        //   const textData = await error.text();
+        //   console.error('Non-JSON Error:', textData);
+        //   return { error: textData };
+        // }
+        // console.error('인증번호 전송 에러:', error);
       }
-    } catch (error) {
-      console.log(error, 131313);
-      // const contentType = error.headers.get('Content-Type') || '';
-      // if (contentType.includes('application/json')) {
-      //   const errorData = await error.json();
-      //   console.error('인증번호 전송 에러:', errorData);
-      //   return errorData;
-      // } else {
-      //   const textData = await error.text();
-      //   console.error('Non-JSON Error:', textData);
-      //   return { error: textData };
-      // }
-      // console.error('인증번호 전송 에러:', error);
     }
   };
 
@@ -147,7 +149,7 @@ export default function FindComponent() {
                 <Timer counter={counter} setCounter={setCounter} />
               </TextField>
               <button
-                onClick={e => {
+                onClick={(e) => {
                   e.preventDefault();
                   // TODO 결과값 받아서 resetComponent 로 route 하기
                   requestCert();
